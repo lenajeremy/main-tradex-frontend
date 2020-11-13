49502fb06c5d2d6bb1ab07d9a953c3de37fb170d
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-11-13 04:17:22 -1100

    'done'

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link} from 'react-router-dom';
 import { editSidebar } from '../actions';
+import {motion} from 'framer-motion';
 import { MailOutline, HomeOutlined, PersonOutlined, ShoppingBasketOutlined, Notifications, NotificationsOutlined, Home, Person, ShoppingBasket, Mail} from '@material-ui/icons';
 import './styles/Sidebar.css';
 
@@ -25,10 +26,10 @@ function Sidebar(props) {
       <div className='sideBar__links right active'>
         <div className='top'></div>
         <div className='bottom'></div>
-        {!userDetails.id ? userDetails.userType === 'buyer' ? <Link onClick = {() => dispatch(editSidebar({ value: 'cart' }))} to='/login'><ShoppingBasket/><span className = 'text'>Cart</span></Link> :
-        <Link onClick = {() => dispatch(editSidebar({ value: 'store' }))} to={'/login'}><ShoppingBasket/><span className = 'text'>Store</span></Link> : 
-      userDetails.userType === 'buyer' ? <Link onClick = {() => dispatch(editSidebar({ value: 'cart' }))} to={'/user/' + userDetails.id.toString() + '/cart'}><ShoppingBasket/><span className = 'text'>Cart<span className = 'products_length'>{products.length}</span></span></Link> :
-      <Link onClick = {() => dispatch(editSidebar({ value: 'store' }))} to={'/user/' + userDetails.id.toString() + '/store'}><ShoppingBasket/><span className = 'text'>Store</span><span className = 'products_length'>{products.length}</span></Link>
+        {!userDetails.id ? userDetails.userType === 'buyer' ? <Link onClick = {() => dispatch(editSidebar({ value: 'cart' }))} to='/login'><ShoppingBasket/><motion.span className = 'text'>Cart</motion.span></Link> :
+        <Link onClick = {() => dispatch(editSidebar({ value: 'store' }))} to={'/login'}><ShoppingBasket/><motion.span className = 'text'>Store</motion.span></Link> : 
+      userDetails.userType === 'buyer' ? <Link onClick = {() => dispatch(editSidebar({ value: 'cart' }))} to={'/user/' + userDetails.id.toString() + '/cart'}><ShoppingBasket/><motion.span className = 'text'>Cart<span className = 'products_length'>{products.length}</span></motion.span></Link> :
+      <Link onClick = {() => dispatch(editSidebar({ value: 'store' }))} to={'/user/' + userDetails.id.toString() + '/store'}><ShoppingBasket/><motion.span className = 'text'>Store</motion.span><span className = 'products_length'>{products.length}</span></Link>
         }
       </div>
       )
@@ -37,10 +38,10 @@ function Sidebar(props) {
       <div className='sideBar__links right'>
         <div className='top'></div>
         <div className='bottom'></div>
-        {!userDetails.id ? userDetails.userType === 'buyer' ? <Link onClick = {() => dispatch(editSidebar({ value: 'cart' }))} to='/login'><ShoppingBasketOutlined/><span className = 'text'>Cart</span></Link> :
-        <Link onClick = {() => dispatch(editSidebar({ value: 'store' }))} to={'/login'}><ShoppingBasketOutlined/><span className = 'text'>Store</span></Link> : 
-    userDetails.userType === 'buyer' ? <Link onClick = {() => dispatch(editSidebar({ value: 'cart' }))} to={'/user/' + userDetails.id.toString() + '/cart'}><ShoppingBasketOutlined/><span className = 'text'>Cart<span className = 'products_length'>{products.length}</span></span></Link> :
-    <Link onClick = {() => dispatch(editSidebar({ value: 'store' }))} to={'/user/' + userDetails.id.toString() + '/store'}><ShoppingBasketOutlined/><span className = 'text'>Store</span><span className = 'products_length'>{products.length}</span></Link>
+        {!userDetails.id ? userDetails.userType === 'buyer' ? <Link onClick = {() => dispatch(editSidebar({ value: 'cart' }))} to='/login'><ShoppingBasketOutlined/><motion.span className = 'text'>Cart</motion.span></Link> :
+        <Link onClick = {() => dispatch(editSidebar({ value: 'store' }))} to={'/login'}><ShoppingBasketOutlined/><motion.span className = 'text'>Store</motion.span></Link> : 
+    userDetails.userType === 'buyer' ? <Link onClick = {() => dispatch(editSidebar({ value: 'cart' }))} to={'/user/' + userDetails.id.toString() + '/cart'}><ShoppingBasketOutlined/><motion.span className = 'text'>Cart<span className = 'products_length'>{products.length}</span></motion.span></Link> :
+    <Link onClick = {() => dispatch(editSidebar({ value: 'store' }))} to={'/user/' + userDetails.id.toString() + '/store'}><ShoppingBasketOutlined/><motion.span className = 'text'>Store</motion.span><span className = 'products_length'>{products.length}</span></Link>
         }
       </div>
     )
@@ -51,23 +52,23 @@ function Sidebar(props) {
         <div className={`sideBar__links left ${active === 'explore' ? 'active' : ''}`}>
             <div className='top'></div>
             <div className='bottom'></div>
-          <Link onClick = {() => dispatch(editSidebar({ value: 'explore' }))} to='/'>{active === 'explore' ? <Home/>: <HomeOutlined/>}<span className = 'text'>Explore</span></Link>
+          <Link onClick = {() => dispatch(editSidebar({ value: 'explore' }))} to='/'>{active === 'explore' ? <Home/>: <HomeOutlined/>}<motion.span className = 'text'>Explore</motion.span></Link>
         </div>
         <div className={`sideBar__links right ${active === 'profile' ? 'active' : ''}`}>
             <div className='top'></div>
             <div className='bottom'></div> 
-          <Link onClick = {() => dispatch(editSidebar({ value: 'profile' }))} to={userDetails.id ? `/user/${userDetails.id}/profile` :'/login'}>{active === 'profile' ? <Person/> : <PersonOutlined/>}<span className = 'text'>Profile</span></Link>
+          <Link onClick = {() => dispatch(editSidebar({ value: 'profile' }))} to={userDetails.id ? `/user/${userDetails.id}/profile` :'/login'}>{active === 'profile' ? <Person/> : <PersonOutlined/>}<motion.span className = 'text'>Profile</motion.span></Link>
         </div>
         <div className={`sideBar__links left ${active === 'account' ? 'active' : ''}`}>
             <div className='top'></div>
             <div className='bottom'></div> 
-          <Link onClick = {() => dispatch(editSidebar({ value: 'account' }))} to={userDetails.id ? `/user/${userDetails.id}/account` : '/login' }>{active === 'account' ? <Mail/> :<MailOutline/>}<span className = 'text'>Chats</span></Link>
+          <Link onClick = {() => dispatch(editSidebar({ value: 'account' }))} to={userDetails.id ? `/user/${userDetails.id}/account` : '/login' }>{active === 'account' ? <Mail/> :<MailOutline/>}<motion.span className = 'text'>Chats</motion.span></Link>
         </div>
         {userTypeDeterminant()}
         <div className={`sideBar__links right ${active === 'notifications' ? 'active' : ''}`}>
             <div className='top'></div>
             <div className='bottom'></div>
-          <Link onClick = {() => dispatch(editSidebar({ value: 'notifications' }))} to='/notifications'>{active === 'notifications' ? <Notifications/>: <NotificationsOutlined/>}<span className = 'text'>Notfcatns</span></Link>
+          <Link onClick = {() => dispatch(editSidebar({ value: 'notifications' }))} to='/notifications'>{active === 'notifications' ? <Notifications/>: <NotificationsOutlined/>}<motion.span className = 'text'>Notfcatns</motion.span></Link>
         </div>
       </div>
     </div>
